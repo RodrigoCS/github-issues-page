a63a4bd6fa5ad3254d32240c8af87934b92888de
Rodrigo Cervantes Saucedo <rod.cervs@gmail.com>
2022-01-16 21:23:27 -0600

    GitHub Pages deployment configuration (#1)
    * Fixed dev/build setup to support GitHub Pages
    * added husky pre-commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import ListView from './views/ListView';
 import DetailView from './views/DetailView';
@@ -6,7 +6,7 @@ import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<MainLayout />}>
           <Route exact path='/' element={<ListView />} />
@@ -16,7 +16,7 @@ function App() {
           />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
